bbf26c05a15399d7c3fbbf82436c85da37c7acf3
Santa0727 <santa0727@outlook.com>
2020-12-13 05:20:18 +0800

    menu items fix

--- app/navigations/MainNavigator.js
+++ app/navigations/MainNavigator.js
@@ -2,13 +2,13 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { FontAwesome } from '@expo/vector-icons';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+import { FontAwesome, MaterialCommunityIcons, FontAwesome5, AntDesign } from '@expo/vector-icons';
 import colors from '../constants/colors';
 import InGameScreen from '../screens/InGameScreen';
 import StartGameScreen from '../screens/StartGameScreen';
 import TruthOrDareScreen from '../screens/TruthOrDareScreen';
-import { LogBox } from 'react-native';
+import { Alert, Linking, LogBox, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
 LogBox.ignoreLogs(['Your project is accessing the following APIs']);
 
@@ -58,6 +58,50 @@ const MainNavigator = createDrawerNavigator({
 			drawerIcon: <FontAwesome name="users" size={24} color='white' />
 		}
 	}
-}, defaultDrawOptions);
+}, {
+	...defaultDrawOptions,
+	contentComponent: props => {
+		const openContactUs = () => {
+			const url = "https://www.sammakaruna.org/teacher-training/tantra-teacher-training-greece/";
+			try {
+				Linking.openURL(url);
+			} catch(err) {
+				Alert.alert('Error!', err.message, [{text: 'OK'}]);
+			}
+		};
+		return (
+			<View style={{ flex: 1, paddingTop: 20 }}>
+				<SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+					<View style={{marginVertical: 30, borderBottomWidth: 1.5, borderBottomColor: '#525150'}}>
+						<Text style={{color: '#3b3a39', marginHorizontal: 10, fontSize: 15}}>GAME</Text>
+						<DrawerItems {...props} />
+					</View>
+					<View style={{marginVertical: 20, borderBottomWidth: 1.5, borderBottomColor: '#525150'}}>
+						<Text style={{color: '#3b3a39', marginHorizontal: 10, fontSize: 15}}>EROTIC DARES</Text>
+						<TouchableOpacity style={{flexDirection: 'row', padding: 10, margin: 5, alignItems: 'center'}}>
+							<FontAwesome name="heart-o" size={26} color="white" />
+							<Text style={{color: 'white', marginLeft: 8, fontWeight: 'bold'}}>Right hand Tantra (soft)</Text>
+						</TouchableOpacity>
+						<TouchableOpacity style={{flexDirection: 'row', padding: 10, margin: 5, alignItems: 'center'}}>
+							<MaterialCommunityIcons name="heart-flash" size={26} color="white" />
+							<Text style={{color: 'white', marginLeft: 8, fontWeight: 'bold'}}>Left hand Tantra (hot)</Text>
+						</TouchableOpacity>
+						<TouchableOpacity style={{flexDirection: 'row', padding: 10, margin: 5, alignItems: 'center'}}>
+							<FontAwesome5 name="hand-holding-heart" size={26} color="white" />
+							<Text style={{color: 'white', marginLeft: 8, fontWeight: 'bold'}}>My Dares</Text>
+						</TouchableOpacity>
+					</View>
+					<View style={{marginTop: 30}}>
+						<TouchableOpacity style={{flexDirection: 'row', padding: 10, margin: 5, alignItems: 'center'}}
+							onPress={openContactUs}>
+							<AntDesign name="contacts" size={24} color="white" />
+							<Text style={{color: 'white', marginLeft: 8, fontWeight: 'bold', fontSize: 12}}>Contact Us</Text>
+						</TouchableOpacity>
+					</View>
+				</SafeAreaView>
+			</View>
+		);
+	}
+});
 
 export default createAppContainer(MainNavigator);
