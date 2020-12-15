f3e90fb6917be1294ba78776ac9fad1ebdf75b6e
Santa0727 <santa0727@outlook.com>
2020-12-15 12:57:13 +0800

    ingame screen add animation

--- app/screens/InGameScreen.js
+++ app/screens/InGameScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, Animated } from 'react-native';
 import { useSelector } from 'react-redux';
+import Swiper from 'react-native-deck-swiper';
 
 import * as GameService from '../services/GameService';
 import colors from '../constants/colors';
@@ -8,11 +10,29 @@ import HeaderToggleMenuButton from '../components/HeaderToggleMenuButton';
 import HeaderLabel from '../components/HeaderLabel';
 import HeaderGoBackButton from '../components/HeaderGoBackButton';
 
+const cardColors = ['#EC2379', '#F5C518', '#F7B402', '#9001F0', '#FAA699', '#0070FF'];
+const localColors = {
+	background: '#fcf',
+	truth: '#AB01FA',
+	dare: '#F52FC5'
+};
+
+const PlayerCard = props => {
+	const width = props.width ?? 150;
+	const height = props.height ?? 100;
+	const { card, cardIndex } = props;
+
+	return (
+		<View style={{...styles.playerCard, width: width, height: height, backgroundColor: cardColors[(cardIndex ?? 0)%cardColors.length]}}>
+			<Text style={styles.cardText}>{card.name}</Text>
+		</View>
+	);
+};
+
 const InGameScreen = props => {
 	const players = useSelector(state => state.game.players);
-	const currentPlayer = players[Math.floor(Math.random() * players.length)];
 	const selectedGameType = useSelector(state => state.game.selectedGameType);
-	const lang = useSelector(state => state.settings.lang);
+	const { lang, getLang } = useSelector(state => state.settings);
 	const { questions, dares } = GameService.QuestionDares(selectedGameType, lang);
 
 	const goToTruth = () => {
@@ -23,24 +43,72 @@ const InGameScreen = props => {
 		const dare = dares[Math.floor(Math.random() * dares.length)];
 		props.navigation.navigate('TruthOrDare', {type: 'dare', question: dare});
 	};
+	const goOpacity = useRef(new Animated.Value(1)).current;
+	const truthDareOpacity = useRef(new Animated.Value(0)).current;
+	const goButtonHide = () => {
+		Animated.timing(goOpacity, {
+			toValue: 0,
+			duration: 700,
+			useNativeDriver: true
+		}).start();
+	};
+	const truthDareShow = () => {
+		Animated.timing(truthDareOpacity, {
+			toValue: 1,
+			duration: 700,
+			useNativeDriver: true
+		}).start();
+	};
+	const swiperRef = useRef();
+	const cardMove = () => {
+		swiperRef.current.swipeRight();
+	};
+	const rotateCount = Math.floor(Math.random() * 20) + 2;
+	const timeout = 400;
+	const rotateByCount = current => {
+		if(current > rotateCount) {
+			goButtonHide();
+			truthDareShow();
+			return;
+		}
+		cardMove();
+		setTimeout(() => {
+			rotateByCount(current + 1);
+		}, timeout);
+	};
+	const rotate = () => {
+		rotateByCount(0);
+	};
 
 	return (
-		<View style={styles.screen}>
-			<View style={styles.title}>
-				<Text style={styles.titleText}>Whoopsie!</Text>
-			</View>
-			<View style={styles.body}>
-				<Text style={styles.bodyText}>It's {currentPlayer.name}</Text>
-			</View>
-			<View style={styles.buttons}>
+		<SafeAreaView style={styles.screen}>
+			<Swiper
+				ref={swiperRef} cards={players}
+				keyExtractor={card => card.id}
+				renderCard={(player, i) => <PlayerCard card={player} cardIndex={i} />}
+				infinite
+				backgroundColor={'transparent'}
+				cardVerticalMargin={50}
+				stackSize={2}
+				animateOverlayLabelsOpacity animateCardOpacity
+				disableTopSwipe={true} disableBottomSwipe={true}
+				disableLeftSwipe={true} disableRightSwipe={true}
+				horizontalSwipe={false} verticalSwipe={false}
+			/>
+			<Animated.View style={{opacity: goOpacity, ...styles.goButtonAnimation}} onTouchEnd={rotate}>
+				<TouchableOpacity styles={styles.goButton}>
+					<Text style={{fontWeight: 'bold', fontSize: 20, color: 'white'}}>{getLang('go')}</Text>
+				</TouchableOpacity>
+			</Animated.View>
+			<Animated.View style={{opacity: truthDareOpacity, ...styles.truthDareButtons}}>
 				<TouchableOpacity style={styles.truth} onPress={goToTruth}>
-					<Text style={styles.truthText}>TRUTH</Text>
+					<Text style={styles.truthText}>{getLang('truth')}</Text>
 				</TouchableOpacity>
 				<TouchableOpacity style={styles.dare} onPress={goToDare}>
-					<Text style={styles.dareText}>DARE</Text>
+					<Text style={styles.dareText}>{getLang('dare')}</Text>
 				</TouchableOpacity>
-			</View>
-		</View>
+			</Animated.View>
+		</SafeAreaView>
 	);
 };
 
@@ -62,59 +130,77 @@ InGameScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
 	screen: {
 		padding: 5,
-		backgroundColor: colors.defaultBackground,
+		backgroundColor: localColors.background,
+		justifyContent: 'center',
+		alignItems: 'center',
 		height: '100%'
 	},
-	header: {
-		marginVertical: 5
-	},
-	titleText: {
-		color: 'white',
-		textAlign: 'center',
-		fontSize: 20,
-		fontWeight: '600'
-	},
-	body: {
-		height: '45%',
+	playerCard: {
+		borderRadius: 12,
+		shadowRadius: 25,
+		shadowColor: 'black',
+		shadowOpacity: 0.08,
+		shadowOffset: { width: 0, height: 0 },
+		justifyContent: 'center',
 		alignItems: 'center',
-		justifyContent: 'center'
+		alignSelf: 'center'
 	},
-	bodyText: {
-		color: 'white',
-		textAlign: 'center',
+	cardText: {
+		fontWeight: 'bold',
 		fontSize: 20,
-		fontWeight: '600'
+		color: 'white'
 	},
-	buttons: {
-		marginVertical: 10
+	goButtonAnimation: {
+		marginTop: 200,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+		backgroundColor: '#F08A00',
+		borderColor: 'white',
+		borderWidth: 1.5,
+	},
+	goButton: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	truthDareButtons: {
+		flexDirection: 'row',
+		padding: 10,
+		justifyContent: 'space-around',
+		width: '100%'
 	},
 	truth: {
-		paddingVertical: 15,
-		backgroundColor: 'white',
-		borderRadius: 10,
-		width: 200,
-		alignSelf: 'center',
+		backgroundColor: localColors.truth,
+		borderRadius: 50,
+		width: 100,
+		height: 100,
+		alignItems: 'center',
+		justifyContent: 'center',
 		marginVertical: 10
 	},
 	truthText: {
 		color: colors.defaultDark,
 		textAlign: 'center',
 		fontSize: 20,
-		fontWeight: '600'
+		fontWeight: 'bold',
+		color: 'white'
 	},
 	dare: {
-		paddingVertical: 15,
-		backgroundColor: colors.defaultDark,
-		borderRadius: 10,
-		width: 200,
-		alignSelf: 'center',
+		backgroundColor: localColors.dare,
+		borderRadius: 50,
+		width: 100,
+		height: 100,
+		alignItems: 'center',
+		justifyContent: 'center',
 		marginVertical: 10
 	},
 	dareText: {
 		color: 'white',
 		textAlign: 'center',
 		fontSize: 20,
-		fontWeight: '600'
+		fontWeight: 'bold'
 	}
 });
 
